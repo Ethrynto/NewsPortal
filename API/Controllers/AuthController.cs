58d69cea4b6c1dad47310c230b5a7eb496e958f4
Ethrynto <ethrynto@outlook.com>
2025-01-07 18:44:23 +0300

    Create a repository interface for the Account

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using API.Contracts;
+using Application.Services;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("/auth")]
+public class AuthController(AccountService accountService): ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(string username, string password)
+    {
+        return Ok();
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody]RegisterAccountRequest request)
+    {
+        accountService.Register(request.Username, request.Email, request.Password);
+        return Ok();
+    }
+}
---
+++ Application/Abstractions/Repositories/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace Application.Abstractions.Repositories;
+
+public interface IAccountRepository
+{
+    void Add(string username, string email, string password);
+    Task<Account> GetById(Guid id);
+    Task<Account> GetByUsername(string username);
+}
