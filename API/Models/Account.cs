c9972037d2ff5666d2827afbf318fa1d26b48fb5
Ethrynto <ethrynto@outlook.com>
2025-01-07 17:12:38 +0300

    Initialize the project

---
+++ API/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models;
+
+public class Account
+{
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+}
---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    // public DbSet<Game> Games { get; set; }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
