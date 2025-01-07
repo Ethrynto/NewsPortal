a09607d45da7724aadc0b81aa4bf9b1bf1ea94d2
Ethrynto <ethrynto@outlook.com>
2025-01-07 18:46:14 +0300

    Configuration for the User model

---
+++ Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        
+        builder.Property(user => user.Username)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(user => user.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+        
+    }
+}
