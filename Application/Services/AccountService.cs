552330ee8bd1cca49b887d78faba7b7772e7c1b8
Ethrynto <ethrynto@outlook.com>
2025-01-07 18:43:22 +0300

    Create a service for the Account

---
+++ Application/Services/AccountService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstractions.Repositories;
+
+namespace Application.Services;
+
+public class AccountService(IAccountRepository accountRepository)
+{
+    public void Register(string username, string email, string password)
+    {
+        //accountRepository.Add(username, password);
+    }
+    
+    public void Login(string username, string email, string password)
+    {
+        //accountRepository.Add(username, password);
+    }
+}
---
+++ Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,38 @@
+﻿using API.Models;
+using Application.Abstractions.Repositories;
+using Application.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class AccountRepository(ApplicationDbContext context) : IAccountRepository
+{
+    public void Add(string username, string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Account> GetById(Guid id)
+    {
+        var user = await context.Users
+            .Where (u => u.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (user != null)
+            return new Account()
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Username = user.Username,
+                PasswordHash = user.Password,
+            };
+        
+        throw new NotFoundException();
+    }
+
+    public Task<Account> GetByUsername(string username)
+    {
+        throw new NotImplementedException();
+    }
+}
