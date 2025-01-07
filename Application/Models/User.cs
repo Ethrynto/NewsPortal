680e277cb3d8d4d30966ad02e50be2714ae2e2ae
Ethrynto <ethrynto@outlook.com>
2025-01-07 18:42:39 +0300

    Create a model for the User

---
+++ Application/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? VerifiedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
