000a476bf27a25f3921a72757b488d26dcbe944c
Ethrynto <ethrynto@outlook.com>
2025-01-07 18:45:43 +0300

    Create an exception for the result when resource not found

---
+++ Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    private const string DefaultMessage = "Resource not found.";
+
+    public NotFoundException()
+        : base(DefaultMessage)
+    {
+    }
+
+    public NotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public NotFoundException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
+
