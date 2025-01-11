f43910cb5e7a781d5f1b76f801f4d857eb7e84bb
Ethrynto <ethrynto@outlook.com>
2025-01-11 16:36:04 +0300

    Change bool to string

--- Domain/Abstractions/Services/IAccountService.cs
+++ Domain/Abstractions/Services/IAccountService.cs
@@ -3,6 +3,6 @@
 public interface IAccountService
 {
     Task<bool> Register(string username, string email, string password);
-    Task<bool> Login(string username, string password);
+    Task<string> Login(string username, string password);
     
 }
