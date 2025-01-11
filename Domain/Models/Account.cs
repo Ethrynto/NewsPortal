9948e1b8c529a8aac3e6cca97114df0e96836840
Ethrynto <ethrynto@outlook.com>
2025-01-11 10:59:14 +0300

    Change the required to nullable

--- Domain/Models/Account.cs
+++ Domain/Models/Account.cs
@@ -5,5 +5,5 @@ public class Account
     public Guid Id { get; set; }
     public required string Username { get; set; }
     public required string Email { get; set; }
-    public required string PasswordHash { get; set; }
+    public string? PasswordHash { get; set; }
 }
