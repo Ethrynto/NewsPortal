6647a5a755f5936c7ca304536ae59c2b33714136
Ethrynto <ethrynto@outlook.com>
2025-01-11 16:09:45 +0300

    Added the logger and controllers api

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,8 @@
+using Application.Services;
+using Domain.Abstractions.Repositories;
+using Domain.Abstractions.Services;
 using Infrastructure;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,13 +12,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("ApplicationDbContext")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services the logging.
+builder.Services.AddLogging(
+    logging =>
+    {
+        logging.ClearProviders(); 
+        logging.AddConsole(); 
+        logging.AddDebug(); 
+        logging.AddEventSourceLogger();
+    });
 
 var app = builder.Build();
+
+// app.UseHttpsRedirection();
+app.UseRouting();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,6 +45,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapGet("/", () => "Hello World!");
 
-app.UseHttpsRedirection();
+app.MapControllers();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>(); 
+logger.LogInformation("Application started");
 app.Run();
