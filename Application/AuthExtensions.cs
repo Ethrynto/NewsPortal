daedc91f6f80837cf869cc628710bc266eebdf95
Ethrynto <ethrynto@outlook.com>
2025-01-11 17:11:50 +0300

    Add auth

---
+++ Application/AuthExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application;
+
+public static class AuthExtensions
+{
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(
+                options =>
+                {
+                    var authSettings = configuration.GetSection("AuthSettings")
+                        .Get<AuthSettings>();
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(authSettings.SecurityKey)),
+                    };
+                });
+        return services;
+    }
+}
