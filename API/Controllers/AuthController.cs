55e3e58fd31fc64d1b917a892cf43e889a6f23a1
Ethrynto <ethrynto@outlook.com>
2025-01-11 16:17:18 +0300

    Realize the methods

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -1,24 +1,38 @@
 ﻿using API.Contracts;
 using Application.Services;
+using Domain.Abstractions.Services;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [ApiController]
-[Route("/auth")]
-public class AuthController(AccountService accountService): ControllerBase
+[Route("[controller]")]
+public class AuthController(IAccountService accountService, ILogger<AuthController> logger)
+    : ControllerBase
 {
+    private readonly ILogger<AuthController> _logger = logger;
+
     [HttpPost("login")]
-    public async Task<IActionResult> Login(string username, string password)
+    public async Task<IActionResult> Login([FromBody] LoginAccountRequest request)
     {
-        return Ok();
+        return Ok(await accountService.Login(request.Username, request.Password));
     }
 
     [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody]RegisterAccountRequest request)
+    public async Task<IActionResult> Register([FromBody] RegisterAccountRequest request)
     {
-        accountService.Register(request.Username, request.Email, request.Password);
-        return Ok();
+        
+        try
+        {
+            await accountService.Register(request.Username, request.Email, request.Password);
+            _logger.LogInformation("The user {Username} successfully registered", request.Username);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The error by user registration {Username}", request.Username); 
+            return StatusCode(500, "Server include errors.");
+        }
     }
 }
