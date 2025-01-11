02d58172de459c314ab14aac78dc223087e68509
Ethrynto <ethrynto@outlook.com>
2025-01-11 10:57:10 +0300

    Exception for the wrong password

---
+++ Domain/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Exceptions;
+
+public class InvalidPasswordException : Exception
+{
+    private const string DefaultMessage = "Wrong password.";
+    
+    public InvalidPasswordException() 
+        : base(DefaultMessage)
+    {
+    }
+
+    public InvalidPasswordException(string message) 
+        : base(message)
+    {
+    }
+
+    public InvalidPasswordException(string message, Exception inner) 
+        : base(message, inner)
+    {
+    }
+}
