e71f5ec96d8e8f0c0e569833eb5855c08294e721
Ethrynto <ethrynto@outlook.com>
2025-01-11 16:15:55 +0300

    Realize the register and login methods

--- Application/Services/AccountService.cs
+++ Application/Services/AccountService.cs
@@ -1,16 +1,39 @@
 ﻿using Domain.Abstractions.Repositories;
+using Domain.Abstractions.Services;
+using Domain.Exceptions;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.Services;
 
-public class AccountService(IAccountRepository accountRepository)
+public class AccountService(IAccountRepository accountRepository) : IAccountService
 {
-    public void Register(string username, string email, string password)
+    public async Task<bool> Register(string username, string email, string password)
     {
-        //accountRepository.Add(username, password);
+        Account account = new Account()
+        {
+            Id = Guid.NewGuid(),
+            Username = username,
+            Email = email
+        };
+        account.PasswordHash = new PasswordHasher<Account>().HashPassword(account, password);
+        return await accountRepository.Add(account);
     }
     
-    public void Login(string username, string email, string password)
+    public async Task<bool> Login(string username, string password)
     {
-        //accountRepository.Add(username, password);
+        Account account = await accountRepository.GetByUsername(username);
+        var isCorrect = new PasswordHasher<Account>().
+            VerifyHashedPassword(account, account.PasswordHash, password);
+
+        if (isCorrect == PasswordVerificationResult.Success)
+        {
+            // Generate token
+        }
+        else
+        {
+            throw new InvalidPasswordException();
+        }
+        return isCorrect > 0;
     }
 }
