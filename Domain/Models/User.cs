c95c397893e50c0b0e4e6e06652890d67d9b0a66
Ethrynto <ethrynto@outlook.com>
2025-01-11 16:16:10 +0300

    Update the User model

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -5,7 +5,7 @@ public class User
     public Guid Id { get; set; }
     public required string Username { get; set; }
     public required string Email { get; set; }
-    public required string Password { get; set; }
+    public required string? Password { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public DateTime? VerifiedAt { get; set; }
