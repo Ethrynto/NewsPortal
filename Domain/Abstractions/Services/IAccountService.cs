49d197304a598e3fc9256de8a0566393a6878a44
Ethrynto <ethrynto@outlook.com>
2025-01-11 16:16:39 +0300

    Create the interface for the AccountService

---
+++ Domain/Abstractions/Services/IAccountService.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Abstractions.Services;
+
+public interface IAccountService
+{
+    Task<bool> Register(string username, string email, string password);
+    Task<bool> Login(string username, string password);
+    
+}
