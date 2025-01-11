abe66c383cdf154da133a6845a4badfbfb87b0a2
Ethrynto <ethrynto@outlook.com>
2025-01-11 16:35:53 +0300

    The service for jwt generate

---
+++ Application/Services/JwtService.cs
@@ -0,0 +1,30 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Services;
+
+public class JwtService(IOptions<AuthSettings> options)
+{
+    public string GenerateToken(Account account)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim("id", account.Id.ToString()),
+            new Claim("username", account.Username),
+            new Claim("email", account.Email),
+        };
+        var jwtToken = new JwtSecurityToken(
+            expires: DateTime.UtcNow.Add(options.Value.Expires),
+            claims: claims,
+            signingCredentials: 
+            new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.Value.SecurityKey)),
+                SecurityAlgorithms.HmacSha256));
+
+        return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+    }
+}
