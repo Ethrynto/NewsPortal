be4602d240224475519b4248d0db6c67f1c7ee3d
Ethrynto <ethrynto@outlook.com>
2025-01-12 18:07:50 +0300

    Added a new field - Role (default, moderator, administrator)

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -5,9 +5,10 @@ public class User
     public Guid Id { get; set; }
     public required string Username { get; set; }
     public required string Email { get; set; }
-    public required string? Password { get; set; }
+    public string? PasswordHash { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? Role { get; set; }
     public IEnumerable<Comment>? Comments { get; set; }
     public DateTime? VerifiedAt { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
