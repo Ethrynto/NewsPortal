3312d50acf1cb92de8e6135d96188bf0d0d6218a
Ethrynto <ethrynto@outlook.com>
2025-01-12 09:39:34 +0300

    Create a model for the Category (for posts)

---
+++ Domain/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Category
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+}
