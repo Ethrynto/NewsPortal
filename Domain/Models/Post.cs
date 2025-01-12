f05513ec5c99525deece7775487356583ac34681
Ethrynto <ethrynto@outlook.com>
2025-01-12 09:47:27 +0300

    Remove author, added the comments field

--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -5,10 +5,9 @@ public class Post
     public Guid Id { get; set; }
     public required string Title { get; set; }
     public string? Content { get; set; }
-    public Guid AuthorId { get; set; }
-    public User? Author { get; set; }
     public Guid CategoryId { get; set; }
     public Category? Category { get; set; }
+    public IEnumerable<Comment>? Comments { get; set; }
     public DateTime PublishedAt { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; }
