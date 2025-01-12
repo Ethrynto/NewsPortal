e80473cfab6dcd2dd96e4d8e6fb6e6b023e9a4fd
Ethrynto <ethrynto@outlook.com>
2025-01-12 10:24:42 +0300

    Create a configuration for a Mediafile

---
+++ Infrastructure/Configurations/MediafileConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class MediafileConfiguration : IEntityTypeConfiguration<Mediafile>
+{
+    public void Configure(EntityTypeBuilder<Mediafile> builder)
+    {
+        builder.ToTable("Mediafiles");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        
+        builder.Property(e => e.FileName)
+            .IsRequired()
+            .HasMaxLength(255);
+        builder.Property(e => e.FileType)
+            .IsRequired()
+            .HasMaxLength(255);
+        
+    }
+    
+}
