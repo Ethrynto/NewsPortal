7efd9177baa4533dd4fcb23ae89123cfeb20d5bd
Ethrynto <ethrynto@outlook.com>
2025-01-12 10:24:54 +0300

    Create a configuration for a Comment

---
+++ Infrastructure/Configurations/CommentConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.ToTable("Comments");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        
+        builder.Property(e => e.Content)
+            .IsRequired()
+            .HasMaxLength(255);
+        
+        builder.HasOne<User>(e => e.User)
+            .WithMany(e => e.Comments)
+            .HasForeignKey(e => e.UserId);
+        
+        builder.HasOne<Post>(e => e.Post)
+            .WithMany(e => e.Comments)
+            .HasForeignKey(e => e.PostId);
+        
+    }
+    
+}
