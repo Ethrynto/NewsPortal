3cccde041e060406d4b5d45d4ffcad3ed773fcdd
Ethrynto <ethrynto@outlook.com>
2025-01-12 10:24:33 +0300

    Create a configuration for a Category

---
+++ Infrastructure/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        
+        builder.Property(e => e.Title)
+            .IsRequired()
+            .HasMaxLength(255);
+        
+        builder.HasMany<Post>(e => e.Posts)
+            .WithOne(e => e.Category)
+            .HasForeignKey(e => e.CategoryId);
+        
+    }
+    
+}
