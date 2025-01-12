3f1fcc715e4daadac5c257dba3640caa497c084e
Ethrynto <ethrynto@outlook.com>
2025-01-12 10:25:15 +0300

    Added the new configurations

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -6,7 +6,11 @@ namespace Infrastructure;
 
 public class ApplicationDbContext : DbContext
 {
+    public DbSet<Mediafile> Mediafiles { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
@@ -15,7 +19,11 @@ public class ApplicationDbContext : DbContext
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new MediafileConfiguration());
         modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new PostConfiguration());
+        modelBuilder.ApplyConfiguration(new CommentConfiguration());
 
         base.OnModelCreating(modelBuilder);
     }
