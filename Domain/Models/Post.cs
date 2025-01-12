5f689e605e57858af3c12e5a659e0e15c75225aa
Ethrynto <ethrynto@outlook.com>
2025-01-12 09:39:42 +0300

    Create a model for the Post

---
+++ Domain/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models;
+
+public class Post
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public string? Content { get; set; }
+    public Guid AuthorId { get; set; }
+    public User? Author { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category? Category { get; set; }
+    public DateTime PublishedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
+}
