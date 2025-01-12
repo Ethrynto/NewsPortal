076b5c58733c4e78cac420f925bb782da7321294
Ethrynto <ethrynto@outlook.com>
2025-01-12 18:02:34 +0300

    Delete a Account model

--- Domain/Models/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Models;
-
-public class Account
-{
-    public Guid Id { get; set; }
-    public required string Username { get; set; }
-    public required string Email { get; set; }
-    public string? PasswordHash { get; set; }
-}
