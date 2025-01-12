8da5006edc1d074c37e829c85a2c3e1ab95c0ae2
Ethrynto <ethrynto@outlook.com>
2025-01-12 09:39:23 +0300

    Create a model for the Mediafile

---
+++ Domain/Models/Mediafile.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models;
+
+public class Mediafile
+{
+    public Guid Id { get; set; }
+    public string? FileName { get; set; }
+    public string? FileType { get; set; }
+    public long FileSize { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
