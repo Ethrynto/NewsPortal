29de2258a7c388ee8f6b6ba8a3e96bcb9ec71a21
Ethrynto <ethrynto@outlook.com>
2025-01-13 05:12:17 +0300

    Create the interface for mediafiles service

---
+++ Domain/Abstractions/Services/IMediafilesService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Domain.Abstractions.Services;
+
+public interface IMediafilesService
+{
+    Task<IEnumerable<Mediafile>> GetAllAsync();
+    Task<Mediafile?> GetByIdAsync(Guid id);
+    Task<Mediafile> AddAsync(IFormFile? image);
+    Task UpdateAsync(Mediafile image);
+    Task DeleteAsync(Guid id);
+}
