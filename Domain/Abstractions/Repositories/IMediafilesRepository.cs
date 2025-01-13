264e02f66a4ccb33ca2b9dea5eb6b669e918b6b5
Ethrynto <ethrynto@outlook.com>
2025-01-13 05:12:06 +0300

    Create the interface for mediafiles repository

---
+++ Domain/Abstractions/Repositories/IMediafilesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions.Repositories;
+
+public interface IMediafilesRepository
+{
+    Task<IEnumerable<Mediafile>> GetAllAsync();
+    Task<Mediafile?> GetByIdAsync(Guid id);
+    Task AddAsync(Mediafile mediafile);
+    Task UpdateAsync(Mediafile mediafile);
+    Task DeleteAsync(Guid id);
+}
