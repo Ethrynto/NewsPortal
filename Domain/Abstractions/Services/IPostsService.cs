dbb7a419d2e49c904cd163e9bbea130cb9dba3ee
Ethrynto <ethrynto@outlook.com>
2025-01-14 06:55:17 +0300

    Create the interface for the posts service

---
+++ Domain/Abstractions/Services/IPostsService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions.Services;
+
+public interface IPostsService
+{
+    Task<IEnumerable<Post>> GetAllAsync();
+    Task<Post> GetByIdAsync(Guid id);
+    Task<Post> CreateAsync(Post post);
+    Task<Post> UpdateAsync(Post post);
+    Task DeleteAsync(Guid id);
+}
