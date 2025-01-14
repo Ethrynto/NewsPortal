4b092776d529290b721d2a46752c26a229b16190
Ethrynto <ethrynto@outlook.com>
2025-01-14 16:24:09 +0300

    Create a controller for the posts

---
+++ API/Controllers/PostsController.cs
@@ -0,0 +1,65 @@
+using API.Contracts;
+using Domain.Abstractions.Services;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PostsController(IPostsService postsService, ILogger<PostsController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetPostsAsync()
+    {
+        logger.LogInformation("Get all posts");
+        return Ok(await postsService.GetAllAsync());
+    }
+
+    // public async Task<IActionResult> GetPostsAsync(int page, int pageSize)
+    // {
+    //     logger.LogInformation("Get all posts");
+    //     return Ok(await postsService.GetAllAsync());
+    // }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPostAsync(Guid id)
+    {
+        logger.LogInformation("Get post by id {id}", id);
+        return Ok(await postsService.GetByIdAsync(id));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreatePostAsync([FromBody] CreatePostRequest request)
+    {
+        logger.LogInformation("Create post");
+        Post post = new Post()
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            Content = request.Content,
+            CategoryId = request.CategoryId,
+        };
+        logger.LogInformation("Create post with id {id}", post.Id);
+        return Ok(await postsService.CreateAsync(post));
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdatePostAsync([FromBody] UpdatePostRequest request)
+    {
+        Post post = await postsService.GetByIdAsync(request.Id);
+        if(request.Title != String.Empty) post.Title = request.Title;
+        if(request.Content != String.Empty) post.Content = request.Content;
+        post.UpdatedAt = DateTime.UtcNow;
+        logger.LogInformation("Update post by id {id}", post.Id);
+        return Ok(await postsService.UpdateAsync(post));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePostAsync(Guid id)
+    {
+        await postsService.DeleteAsync(id);
+        logger.LogInformation("Delete post by id {id}", id);
+        return Ok();
+    }
+}
