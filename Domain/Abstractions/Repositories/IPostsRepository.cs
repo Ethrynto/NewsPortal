4acb95f48258a1644c65c83b819e0448ddb03126
Ethrynto <ethrynto@outlook.com>
2025-01-14 06:55:11 +0300

    Create the interface for the posts repository

---
+++ Domain/Abstractions/Repositories/IPostsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions.Repositories;
+
+public interface IPostsRepository
+{
+    Task<IEnumerable<Post>> GetAllAsync();
+    Task<Post> GetByIdAsync(Guid id);
+    Task<Post> CreateAsync(Post post);
+    Task<Post> UpdateAsync(Post post);
+    Task DeleteAsync(Guid id);
+}
