7e3fc0beeee54f200b614bb89fb4d9fed4d4cf03
Ethrynto <ethrynto@outlook.com>
2025-01-14 06:55:39 +0300

    Create the service for the posts

---
+++ Application/Services/PostsService.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Abstractions.Repositories;
+using Domain.Abstractions.Services;
+using Domain.Models;
+
+namespace Application.Services;
+
+public class PostsService(IPostsRepository repository) : IPostsService
+{
+    public async Task<IEnumerable<Post>> GetAllAsync()
+    {
+        return await repository.GetAllAsync();
+    }
+
+    public async Task<Post> GetByIdAsync(Guid id)
+    {
+        return await repository.GetByIdAsync(id);
+    }
+
+    public async Task<Post> CreateAsync(Post post)
+    {
+        return await repository.CreateAsync(post);
+    }
+
+    public async Task<Post> UpdateAsync(Post post)
+    {
+        return await repository.UpdateAsync(post);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await repository.DeleteAsync(id);
+    }
+}
