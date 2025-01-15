e980f06767e5c363215c97c6a3f7c2a3431b4a82
Ethrynto <ethrynto@outlook.com>
2025-01-15 15:03:24 +0300

    Added an interface for the comment service

---
+++ Domain/Abstractions/Services/ICommentsService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions.Services;
+
+public interface ICommentsService
+{
+    Task<IEnumerable<Comment>> GetAllAsync();
+    Task<Comment> GetByIdAsync(Guid id);
+    Task<Comment> CreateAsync(Comment comment);
+    Task<Comment> UpdateAsync(Comment comment);
+    Task DeleteAsync(Guid id);
+}
