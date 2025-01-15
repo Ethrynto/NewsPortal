b048ee48484027046f006a27a2c39c8242e4336f
Ethrynto <ethrynto@outlook.com>
2025-01-15 15:03:43 +0300

    Added a service for the comments

---
+++ Application/Services/CommentsService.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Abstractions.Repositories;
+using Domain.Abstractions.Services;
+using Domain.Models;
+
+namespace Application.Services;
+
+public class CommentsService(ICommentsRepository repository) : ICommentsService
+{
+    public async Task<IEnumerable<Comment>> GetAllAsync()
+    {
+        return await repository.GetAllAsync();
+    }
+
+    public async Task<Comment> GetByIdAsync(Guid id)
+    {
+        return await repository.GetByIdAsync(id);
+    }
+
+    public async Task<Comment> CreateAsync(Comment comment)
+    {
+        return await repository.CreateAsync(comment);
+    }
+
+    public async Task<Comment> UpdateAsync(Comment comment)
+    {
+        return await repository.UpdateAsync(comment);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await repository.DeleteAsync(id);
+    }
+}
